33cf84ee696bcb649021feb342e727aeb18a040d
hamrithasree <hamritha18@gmail.com>
2024-12-30 17:28:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <Router basename="/react-deploy">
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      <Footer/>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
