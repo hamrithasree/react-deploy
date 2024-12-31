ad241b1d43ed8e9b29cf280f9e068b41a9ba4299
hamrithasree <hamritha18@gmail.com>
2024-12-31 14:44:55 +0530

    video

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,7 +9,14 @@ const About = () => {
         <h1>About</h1>
         <p>Your one-stop solution for everything.</p>
       </div>
-      
+      <section class="bg-light py-5 text-center">
+        <div id="Watch Now" class="container">
+            <h2>Watch Now</h2>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/6knw50WVhGY?autoplay=1&si=ImtqMhfZ0jANWj2G" width="1000" height="500" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
 
     </div>
   );
